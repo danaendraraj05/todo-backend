03e942947d1c9087eac8f103c079f83a51226310
Danaendraraj R <danaendraraj5@gmail.com>
2025-07-28 06:44:40 +0000

    Add serializers for project , todo and comments

---
+++ app/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from app.models import Project, Todo, Comment
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    assignees = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
+
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+class TodoSerializer(serializers.ModelSerializer):
+    assignees = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
+
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+class TodoCommentSerializer(serializers.ModelSerializer):
+    author = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
