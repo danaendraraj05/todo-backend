28ecd29ccd7a42dd3e383ed8a598400dd5966781
Danaendraraj R <danaendraraj5@gmail.com>
2025-07-28 07:01:04 +0000

    Modify model and add migration files

--- app/models/project.py
+++ app/models/project.py
@@ -5,6 +5,7 @@ class Project(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_projects', null=True)
     assignees = models.ManyToManyField(User, related_name='project_assignees')
     is_completed = models.BooleanField(default=False)
 
